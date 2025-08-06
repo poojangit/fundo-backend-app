c8e6e15ea45cf7451ebd4c0c302d9fb26a3cd53d
poojangit <poojang147@gmail.com>
2025-08-06 23:46:50 +0530

    [POOJA] Refactored : Refactored the config file, added formatted password reset email and Updated sendMail function to include proper formatting for plain text and HTML emails

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,138 +1,136 @@
-import HttpStatus from 'http-status-codes'
-import bcrypt from 'bcrypt'
-import User from '../models/user.model'
-import jwt from 'jsonwebtoken'
-import { sendMail } from '../utils/mailer.util'
-const saltRounds = 10
+import HttpStatus from 'http-status-codes';
+import bcrypt from 'bcrypt';
+import User from '../models/user.model';
+import jwt from 'jsonwebtoken';
+import { sendMail } from '../config/email';
+const saltRounds = 10;
 //Create new User
-export const newUser = async(body) => {
-    try {
-        // check if user already exists
-        const checkUser = await User.findOne({
-        email : body.email
-    })
-    if(checkUser){
-        return {
-            code : HttpStatus.CONFLICT,
-            data : [],
-            message : "User already exists"
-        }
+export const newUser = async (body) => {
+  try {
+    // check if user already exists
+    const checkUser = await User.findOne({
+      email: body.email
+    });
+    if (checkUser) {
+      return {
+        code: HttpStatus.CONFLICT,
+        data: [],
+        message: 'User already exists'
+      };
     }
-    //hash password 
-    const hashedPass = await bcrypt.hash(body.password, saltRounds)
+    //hash password
+    const hashedPass = await bcrypt.hash(body.password, saltRounds);
 
     const data = await User.create({
-        ...body,
-        password: hashedPass
-    })
+      ...body,
+      password: hashedPass
+    });
 
     return {
-        code : HttpStatus.CREATED,
-        data,
-        message : "User created successfully!"
-    }
-    } catch(error) {
-        console.error(error);
-        return {
-            code : HttpStatus.INTERNAL_SERVER_ERROR,
-            data : [],
-            message : "Error occured while creating user"
-        } 
-    }
-}
+      code: HttpStatus.CREATED,
+      data,
+      message: 'User created successfully!'
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      code: HttpStatus.INTERNAL_SERVER_ERROR,
+      data: [],
+      message: 'Error occured while creating user'
+    };
+  }
+};
 
-export const userLogin = async({email, password}) => {
-    try {
-        const checkUser = await User.findOne({
-                email : email
-        })
-        console.log("User email ", checkUser);
-        if(!checkUser){
-            return {
-                code : HttpStatus.NOT_FOUND,
-                data : [],
-                message : "no user found"
-            }
-        }
-
-        // Compare password
-        const isMatch = await bcrypt.compare(password, checkUser.password)
-        if(!isMatch) {
-            return {
-                code : HttpStatus.UNAUTHORIZED,
-                data : [],
-                message : "Invalid Credentials"
-            }
-        }
-        const token = jwt.sign({ email: checkUser.email, id : checkUser._id }, process.env.ACCESS_TOKEN_KEY);
-
-        // return success
-        return {
-            code : HttpStatus.OK,
-            data : {
-                id : checkUser._id,
-                email: checkUser.email,
-                token : token
-            },
-            message : "Login successful and token generated"
-        }
+export const userLogin = async ({ email, password }) => {
+  try {
+    const checkUser = await User.findOne({
+      email: email
+    });
+    console.log('User email ', checkUser);
+    if (!checkUser) {
+      return {
+        code: HttpStatus.NOT_FOUND,
+        data: [],
+        message: 'no user found'
+      };
     }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, checkUser.password);
+    if (!isMatch) {
+      return {
+        code: HttpStatus.UNAUTHORIZED,
+        data: [],
+        message: 'Invalid Credentials'
+      };
+    }
+    const token = jwt.sign(
+      { email: checkUser.email, id: checkUser._id },
+      process.env.ACCESS_TOKEN_KEY
+    );
+
+    // return success
+    return {
+      code: HttpStatus.OK,
+      data: {
+        id: checkUser._id,
+        email: checkUser.email,
+        token: token
+      },
+      message: 'Login successful and token generated'
+    };
+  } catch (error) {
     //login error catch
-    catch (error) {
-        console.error(error)
-        return  {
-            code : HttpStatus.INTERNAL_SERVER_ERROR,
-            data : [],
-            message : "Error occured during login"
-        }
+    console.error(error);
+    return {
+      code: HttpStatus.INTERNAL_SERVER_ERROR,
+      data: [],
+      message: 'Error occured during login'
+    };
+  }
+};
+
+export const forgotPass = async ({ email }) => {
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return {
+        code: HttpStatus.NOT_FOUND,
+        data: [],
+        message: 'No user found'
+      };
     }
-}
+    //Generate a token
+    const token = jwt.sign(
+      { id: checkUser._id, email: checkUser.email },
+      process.env.ACCESS_TOKEN_KEY,
+      { expiresIn: '15m' }
+    );
+    console.log('Generated Reset Token: ', token);
 
-export const forgotPass = async({email}) => {
-    try {
-        const checkUser = await User.findOne({email})
-        if(!checkUser) {
-            return {
-                code : HttpStatus.NOT_FOUND,
-                data : [],
-                message : "No user found"
-            }
-        }
-        //Generate a token
-        const token = jwt.sign(
-            {id: checkUser._id, email: checkUser.email},
-            process.env.ACCESS_TOKEN_KEY,
-            {expiresIn : '15m'}
-        )
-        console.log("Generated Reset Token: ", token);
+    //TODO: here ----> Send token via email to the user using nodemailer
 
-        //TODO: here ----> Send token via email to the user using nodemailer
-
-        //send token via mail
-        const emailSent = await sendMail(
-            email,
-            "Password Reset Request",
-            `Here is your password  reset token: ${token}`
-        )
-        if(!emailSent) {
-            return {
-                code : HttpStatus.INTERNAL_SERVER_ERROR,
-                data : [],
-                message : "Failed to send reset email"
-            }
-        }
-
-        return {
-            code : HttpStatus.OK,
-            data : {token},
-            message : "Password reser token generated successfully!!"
-        }
-    } catch(error) {
-        console.error("Error in forgot password: ", error);
-        return {
-            code : HttpStatus.INTERNAL_SERVER_ERROR,
-            data : [],
-            message : "Error Occured during forgot password!"
-        }
+    //send token via mail
+    const emailSent = await sendMail(email,token);
+    if (!emailSent) {
+      return {
+        code: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: [],
+        message: 'Failed to send reset email'
+      };
     }
-}
+    
+    return {
+      code: HttpStatus.OK,
+      data: { token },
+      message: 'Password reset token generated successfully!!'
+    };
+  } catch (error) {
+    console.error('Error in forgot password: ', error);
+    return {
+      code: HttpStatus.INTERNAL_SERVER_ERROR,
+      data: [],
+      message: 'Error Occured during forgot password!'
+    };
+  }
+};
